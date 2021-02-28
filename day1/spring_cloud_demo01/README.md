c51a9ca62d9106ac9d2272f61eb8d10890589e58
raiden <peckwood@users.noreply.github.com>
2021-02-28 19:11:35 +0800

    day01/spring_cloud_demo01: achieved using RestTemplate to send requests between 'microservices'

--- day1/spring_cloud_demo01/README.md
+++ day1/spring_cloud_demo01/README.md
@@ -3,3 +3,10 @@
 ## 模拟微服务的项目
 
 微服务之间的调用通过RestTemplate实现
+
+### 如何运行
+
+1. 开启mysql
+2. 开启product_service
+3. 开启order_service
+4. GET访问localhost:9002/order/buy/1 (postman里有)
--- day1/spring_cloud_demo01/order_service/src/main/java/cn/itcast/order/controller/OrderController.java
+++ day1/spring_cloud_demo01/order_service/src/main/java/cn/itcast/order/controller/OrderController.java
@@ -1,11 +1,15 @@
 package cn.itcast.order.controller;
 
 import cn.itcast.order.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("order")
 public class OrderController{
+    @Autowired
+    private RestTemplate restTemplate;
     /**
      * 通过订单系统
      * 根据product id 查询product
@@ -13,6 +17,8 @@ public class OrderController{
      */
     @GetMapping("buy/{id}")
     public Product findProductById(@PathVariable Long id){
-        return null;
+        String url = "http://localhost:9001/product/"+ id;
+        Product product = restTemplate.getForObject(url, Product.class);
+        return product;
     }
 }
