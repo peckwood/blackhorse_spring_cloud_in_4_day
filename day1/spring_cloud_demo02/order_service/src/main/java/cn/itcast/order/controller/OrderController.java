89e20e93f8bfc7e2839ddfdabf6a6d13fedc9829
raiden <peckwood@users.noreply.github.com>
2021-02-28 19:39:27 +0800

    day1/spring_cloud_demo02: copied from spring_cloud_demo01

---
+++ day1/spring_cloud_demo02/order_service/src/main/java/cn/itcast/order/controller/OrderController.java
@@ -0,0 +1,24 @@
+package cn.itcast.order.controller;
+
+import cn.itcast.order.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("order")
+public class OrderController{
+    @Autowired
+    private RestTemplate restTemplate;
+    /**
+     * 通过订单系统
+     * 根据product id 查询product
+     * @param id product id
+     */
+    @GetMapping("buy/{id}")
+    public Product findProductById(@PathVariable Long id){
+        String url = "http://localhost:9001/product/"+ id;
+        Product product = restTemplate.getForObject(url, Product.class);
+        return product;
+    }
+}
