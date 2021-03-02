226aa5430983be30a6efc2e08944f0d44c9bde7e
raiden <peckwood@users.noreply.github.com>
2021-03-02 23:25:23 +0800

    day1/spring_cloud_demo02: finished service provider ProductApplication

--- day1/spring_cloud_demo02/product_service/src/main/java/cn/itcast/product/ProductApplication.java
+++ day1/spring_cloud_demo02/product_service/src/main/java/cn/itcast/product/ProductApplication.java
@@ -3,9 +3,11 @@ package cn.itcast.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EntityScan("cn.itcast.product.entity")
+@EnableDiscoveryClient
 public class ProductApplication{
     public static void main(String[] args){
         SpringApplication.run(ProductApplication.class, args);
