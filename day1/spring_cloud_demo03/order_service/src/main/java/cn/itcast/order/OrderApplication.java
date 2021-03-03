f75938cd502e800836ee427f9b263454a33683b0
raiden <peckwood@users.noreply.github.com>
2021-03-03 22:31:08 +0800

    day1/spring_cloud_demo03: copied from spring_cloud_demo02, needs modification

---
+++ day1/spring_cloud_demo03/order_service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,22 @@
+package cn.itcast.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EntityScan("cn.itcast.order.entity")
+@EnableDiscoveryClient
+public class OrderApplication{
+    public static void main(String[] args){
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
