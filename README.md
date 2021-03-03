9fe668f3cdd27cf5f75483ca4331fd7141e257a4
raiden <peckwood@users.noreply.github.com>
2021-03-03 21:46:54 +0800

    day1/spring_cloud_demo02: finished service consumer OrderApplication

--- README.md
+++ README.md
@@ -4,7 +4,20 @@
 
 Emulating microservices by creating 2 Spring boot web apps and use RestTemplate to communicate.
 
-模拟微服务
+通过RestTemplate调用模拟微服务
 
+对应的视频: 
 
+- 10-模拟微服务环境-上
+- 11-模拟微服务环境-下
+- 12-模拟微服务环境：通过RestTemplate调用远程服务
 
+### spring_cloud_demo02
+
+加入EurekaServer, 通过discoveryClient获取微服务的调用路径, 仍用restTemplate调用
+
+对应的视频: 
+
+- 16-eureka：搭建EurekaServer注册中心
+- 17-eureka：将服务注册到注册中心
+- 18-eureka：eureka中通过元数据获取微服务的调用路径
--- day1/spring_cloud_demo02/order_service/src/main/java/cn/itcast/order/OrderApplication.java
+++ day1/spring_cloud_demo02/order_service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -2,10 +2,14 @@ package cn.itcast.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EntityScan("cn.itcast.order.entity")
+@EnableDiscoveryClient
 public class OrderApplication{
     public static void main(String[] args){
         SpringApplication.run(OrderApplication.class, args);
