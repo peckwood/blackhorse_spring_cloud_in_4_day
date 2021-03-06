39a9d50ba198d7aadf516bbbb78f635cf3c9dad6
Feng Xiao <peckwood@users.noreply.github.com>
2021-03-06 16:44:59 +0800

    day1/spring_cloud_demo03: finished ImportSelector

--- day1/spring_cloud_demo03/import_test/src/main/java/cn/itcast/bean/UserImportSelector.java
+++ day1/spring_cloud_demo03/import_test/src/main/java/cn/itcast/bean/UserImportSelector.java
@@ -1,11 +1,20 @@
 package cn.itcast.bean;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.MethodMetadata;
 
+import java.util.Set;
+
+@Slf4j
 public class UserImportSelector implements ImportSelector{
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata){
+        Set<String> annotationTypes = importingClassMetadata.getAnnotationTypes();
+        log.info("annotationTypes: {}", annotationTypes);
+
         //获取配置类的名称
         return new String[]{(UserConfiguration.class.getName())};
     }
