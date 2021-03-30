4b47183dcd3333e831b09ea8032d624c37ffd3a4
raiden <peckwood@users.noreply.github.com>
2021-03-30 23:39:13 +0800

    spring_cloud_demo_06_hello_feign: working

--- day2/spring_cloud_demo_06_hello_feign/order_service/src/main/java/cn/itcast/order/controller/OrderController.java
+++ day2/spring_cloud_demo_06_hello_feign/order_service/src/main/java/cn/itcast/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package cn.itcast.order.controller;
 
 import cn.itcast.order.entity.Product;
+import cn.itcast.order.feign.ProductFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -12,34 +13,12 @@ import java.util.List;
 @RestController
 @RequestMapping("order")
 public class OrderController{
+
     @Autowired
-    private RestTemplate restTemplate;
+    private ProductFeignClient productFeignClient;
 
-    /**
-     * 注入DiscoveryClient:
-     * springcloud提供的获取原数据的工具类
-     * 调用方法获取服务的元数据信息
-     */
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-
-    /**
-     * 通过订单系统
-     * 根据product id 查询product
-     * @param id product id
-     */
     @GetMapping("buy/{id}")
     public Product findProductById(@PathVariable Long id){
-        // 调用discoveryClient方法
-        //以调用服务名称获取所有的元数据
-        List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
-
-        //获取唯一的一个元数据
-        ServiceInstance instance = instances.get(0);
-        //根据元数据中的主机地址和端口号拼接请求
-        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/product/" + id;
-        Product product = restTemplate.getForObject(url, Product.class);
-        return product;
+        return productFeignClient.findById(id);
     }
 }
