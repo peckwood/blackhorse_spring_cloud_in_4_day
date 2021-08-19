3ff731928f5708b974d0a7c99daab05b72898675
Feng Xiao <peckwood@users.noreply.github.com>
2021-08-19 22:42:03 +0800

    开启动态刷新

--- day4/hello_spring_cloud_config/product_service/src/main/java/cn/itcast/product/controller/ProductController.java
+++ day4/hello_spring_cloud_config/product_service/src/main/java/cn/itcast/product/controller/ProductController.java
@@ -4,10 +4,16 @@ import cn.itcast.product.entity.Product;
 import cn.itcast.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("product")
+//开启动态刷新
+@RefreshScope
 public class ProductController{
     @Autowired
     private ProductService productService;
